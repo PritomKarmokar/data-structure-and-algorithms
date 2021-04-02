d4f40ca0ecbe2e687d52fbab5adf8f795e2176a6
Pritom Karmokar <pritomkarmokar99@gmail.com>
2021-04-03 03:30:55 +0600

    linked list data structure is implemented in this directory

---
+++ linked_list/implementation using structure.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct Node{
+    int data;
+    Node *next;
+};
+
+Node *create_node(int, Node*);
+
+int main()
+{
+    Node *head = NULL;
+
+    Node *n1 = create_node(10, NULL);
+
+    head = n1;
+
+    cout<<head->data<<endl;
+    
+}
+
+Node *create_node(int data, Node* next)
+{
+    Node *new_node = new Node();
+
+    if(new_node == NULL){
+        cout<<"Error! Could not create a new node!\n";
+        exit(1);
+    }
+
+    new_node->data = data;
+    new_node->next = next;
+
+    return new_node;
+}
+
